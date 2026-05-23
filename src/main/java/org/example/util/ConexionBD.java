9bcc44d62bcf182353a0422fa956ecd413884e1b
BSXQ-0 <158426085+BSXQ-0@users.noreply.github.com>
2026-05-22 19:51:52 -0500

    Subiendo

--- src/main/java/org/example/util/ConexionBD.java
+++ src/main/java/org/example/util/ConexionBD.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3314/concesionario";
+    private static final String URL = "jdbc:mysql://localhost:3314/papeleria_db";
     private static final String USUARIO = "root";
     private static final String CONTRASENA = "1234";
 
